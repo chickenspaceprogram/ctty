5aac952408051bdca323a3d1ae13b3e451406c78
Athena Boose <pestpestthechicken@yahoo.com>
2024-10-31 14:02:14 -0700

    Added all cursor positioning functions.

--- ansi/cursor.h
+++ ansi/cursor.h
@@ -8,12 +8,24 @@ typedef struct position {
     int col;
 } Position;
 
-#define CURSOR_HOME
-#define CURSOR_TO_POSITION(row, col)    printf("\x1B[%d;%dH", row, col) // Moves cursor to position (row, col).
+#define CURSOR_HOME                     "\x1B[H" // moves the cursor to position (0, 0)
+
 #define CURSOR_UP(rows)                 printf("\x1B[%dA", rows) // Moves cursor up by `rows` lines.
 #define CURSOR_DOWN(rows)               printf("\x1B[%dB", rows) // Moves cursor down by `rows` lines.
-#define CURSOR_LEFT
-#define CURSOR_RIGHT
+#define CURSOR_RIGHT(cols)              printf("\x1B[%dC", cols) // Moves cursor right by `cols` lines.
+#define CURSOR_LEFT(cols)               printf("\x1B[%dD", cols) // Moves cursor left by `cols` lines.
+
+#define CURSOR_DOWN_LINE_START(rows)    printf("\x1B[%dE", rows) // moves the cursor down by `rows` lines and to the start of the line it lands on
+#define CURSOR_UP_LINE_START(rows)      printf("\x1B[%dF", rows) // moves the cursor up by `rows` lines and to the start of the line it lands on
+#define CURSOR_UP_SCROLL                "\x1B M" // Moves cursor up one line, scrolling if needed
+
+#define CURSOR_TO_COL(col)              printf("\x1B[%dG", col) // Moves cursor to column `col`.
+#define CURSOR_TO_POSITION(row, col)    printf("\x1B[%d;%dH", row, col) // Moves cursor to position (row, col).
+#define CURSOR_GET_POSITION()           get_cursor_position(void) // Gets the current cursor position and returns it as a `Position` struct.
+
+#define SAVE_CURSOR                     "\x1B 7" // Saves the cursor's position
+#define LOAD_CURSOR                     "\x1B 8" // Loads the cursor from the saved position in memory
+
 
 
 /**
--- escape-codes.h
+++ escape-codes.h
@@ -9,14 +9,13 @@
 
 #define HIDE_CURSOR                     "\x1B[?25l" // Hides the cursor
 #define SHOW_CURSOR                     "\x1B[?25h" // Shows the cursor
-#define SAVE_CURSOR                     "\x1B 7" // Saves the cursor's position
-#define LOAD_CURSOR                     "\x1B 8" // Loads the cursor from the saved position in memory
+
 
 // macros for foreground and background colors
 
 
 
-#define CURSOR_TO_POSITION(row, col)    printf("\x1B[%d;%dH", row, col) // Moves cursor to position (row, col).
-#define CURSOR_TO_COL(col)              printf("\x1B[%dG", col) // Moves cursor to column `col`.
+
+
 
 #endif
