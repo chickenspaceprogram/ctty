0d1292ccd5f98f09602080d31d824ed6fb2f1f2b
Athena Boose <pestpestthechicken@yahoo.com>
2024-11-03 10:40:53 -0800

    Silenced some warnings resulting from not explicitly typecasting.

--- keypress-codes.h
+++ keypress-codes.h
@@ -7,50 +7,50 @@
 #ifndef KEYPRESS_CODES_H
 #define KEYPRESS_CODES_H
 
-#define CTTY_ESC        {"\x1B", 1}
+#define CTTY_ESC        {(unsigned char *)"\x1B", 1}
 
-#define CTTY_a          {"a", 1}
-#define CTTY_b          {"b", 1}
-#define CTTY_c          {"c", 1}
-#define CTTY_d          {"d", 1}
-#define CTTY_e          {"e", 1}
-#define CTTY_f          {"f", 1}
-#define CTTY_g          {"g", 1}
-#define CTTY_h          {"h", 1}
-#define CTTY_i          {"i", 1}
-#define CTTY_j          {"j", 1}
+#define CTTY_a          {(unsigned char *)"a", 1}
+#define CTTY_b          {(unsigned char *)"b", 1}
+#define CTTY_c          {(unsigned char *)"c", 1}
+#define CTTY_d          {(unsigned char *)"d", 1}
+#define CTTY_e          {(unsigned char *)"e", 1}
+#define CTTY_f          {(unsigned char *)"f", 1}
+#define CTTY_g          {(unsigned char *)"g", 1}
+#define CTTY_h          {(unsigned char *)"h", 1}
+#define CTTY_i          {(unsigned char *)"i", 1}
+#define CTTY_j          {(unsigned char *)"j", 1}
 
-#define CTTY_1          {"1", 1}
-#define CTTY_2          {"2", 1}
-#define CTTY_3          {"3", 1}
-#define CTTY_4          {"4", 1}
-#define CTTY_5          {"5", 1}
-#define CTTY_6          {"6", 1}
-#define CTTY_7          {"7", 1}
-#define CTTY_8          {"8", 1}
-#define CTTY_9          {"9", 1}
-#define CTTY_0          {"0", 1}
+#define CTTY_1          {(unsigned char *)"1", 1}
+#define CTTY_2          {(unsigned char *)"2", 1}
+#define CTTY_3          {(unsigned char *)"3", 1}
+#define CTTY_4          {(unsigned char *)"4", 1}
+#define CTTY_5          {(unsigned char *)"5", 1}
+#define CTTY_6          {(unsigned char *)"6", 1}
+#define CTTY_7          {(unsigned char *)"7", 1}
+#define CTTY_8          {(unsigned char *)"8", 1}
+#define CTTY_9          {(unsigned char *)"9", 1}
+#define CTTY_0          {(unsigned char *)"0", 1}
 
 
 #ifdef _WIN32
 
-#define CTTY_UP         {"\xE0H", 2} // up arrow key
-#define CTTY_DOWN       {"\xE0P", 2} // down arrow key
-#define CTTY_LEFT       {"\xE0K", 2} // left arrow key
-#define CTTY_RIGHT      {"\xE0M", 2} // right arrow key
-#define CTTY_ENTER      {"\r", 1} // [Enter] key
+#define CTTY_UP         {(unsigned char *)"\xE0H", 2} // up arrow key
+#define CTTY_DOWN       {(unsigned char *)"\xE0P", 2} // down arrow key
+#define CTTY_LEFT       {(unsigned char *)"\xE0K", 2} // left arrow key
+#define CTTY_RIGHT      {(unsigned char *)"\xE0M", 2} // right arrow key
+#define CTTY_ENTER      {(unsigned char *)"\r", 1} // [Enter] key
 
-#define CTTY_DEL        {"\xE0S", 2}
+#define CTTY_DEL        {(unsigned char *)"\xE0S", 2}
 
 #else
 
-#define CTTY_UP         {"\x1B[A", 3} // up arrow key
-#define CTTY_DOWN       {"\x1B[B", 3} // down arrow key
-#define CTTY_LEFT       {"\x1B[D", 3} // left arrow key
-#define CTTY_RIGHT      {"\x1B[C", 3} // right arrow key
-#define CTTY_ENTER      {"\n", 1} // [Enter] key
+#define CTTY_UP         {(unsigned char *)"\x1B[A", 3} // up arrow key
+#define CTTY_DOWN       {(unsigned char *)"\x1B[B", 3} // down arrow key
+#define CTTY_LEFT       {(unsigned char *)"\x1B[D", 3} // left arrow key
+#define CTTY_RIGHT      {(unsigned char *)"\x1B[C", 3} // right arrow key
+#define CTTY_ENTER      {(unsigned char *)"\n", 1} // [Enter] key
 
-#define CTTY_DEL        {"\x1B[3~", 4}
+#define CTTY_DEL        {(unsigned char *)"\x1B[3~", 4}
 
 #endif
 #endif
